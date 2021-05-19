1367b7085dc0b67d3334bc554489d83c83e889b5
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-05-19 11:31:41 +0700

    Su dung chunkById

--- app/Console/Commands/GenerateAvatar.php
+++ app/Console/Commands/GenerateAvatar.php
@@ -32,15 +32,30 @@ class GenerateAvatar extends Command
     {
         $this->info('Lấy danh sách người dùng chưa có avatar...');
 
-        $userList = User::with('organization')
-            ->whereNull('avatar')
+        /*
+        $userList = User::whereNull('avatar')
             ->orderBy('id', 'desc')
             ->get();
 
         foreach ($userList as $user) {
-            $fullName = $user->full_name;
-            $this->info($fullName);
-            AvatarGenerator::autoGenerateAvatar($user);
+            // AvatarGenerator::autoGenerateAvatar($user);
         }
+        */
+
+        // Giả sử tổng số bản ghi nhiều mà chúng ta cần duyệt tất cả các bản ghi
+        // thì thay vì select tất cả các bản ghi một lần, có thể gây hết bộ nhớ,
+        // chúng ta nên sử dụng chunkById để xử lý từng phần
+        // Ngoài ra, cũng nên chỉ select các trường cần sử dụng
+        // Làm thế thì khi update, các trường không được select có bị update thành null không?
+        $count = 1;
+        User::select(['id', 'full_name'])
+            ->chunkById(10, function ($userList) use (&$count) {
+                // Phải thêm ký tự & thì mới có thể thay đổi biến $count trong closure
+                $this->info($count);
+                $count++;
+                foreach ($userList as $user) {
+                    // $this->info($user->full_name);
+                }
+            });
     }
 }
